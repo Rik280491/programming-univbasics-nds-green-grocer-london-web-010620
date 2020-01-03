9c614c7366ccd59f92928b8630c8545645a49e6b
Rikesh Balakrishnan <rik280491@googlemail.com>
2020-01-03 15:37:30 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -67,5 +67,10 @@ def apply_clearance(cart)
 end
 
 def checkout(cart, coupons)
+  consolidated_cart = consolidate_cart(cart)
+  couponed_cart = apply_coupons(consolidated_cart, coupons)
+  final_cart = apply_clearance(couponed_cart)
+  
+  total = 0 
   
 end
